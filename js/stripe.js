14a05c1116513fe0a4658e3fec49baa5ad20005b
Marc Morera <yuhu@mmoreram.com>
2019-09-25 21:20:08 +0200

    Fixed links

--- js/stripe.js
+++ js/stripe.js
@@ -32,29 +32,27 @@
     var checkoutButtonsRegular = document.getElementsByClassName('checkout-button-regular');
     Array.prototype.forEach.call(checkoutButtonsRegular, function(button) {
         button.addEventListener('click', function () {
-            button.addEventListener('click', function () {
-                // When the customer clicks on the button, redirect
-                // them to Checkout.
-                stripe.redirectToCheckout({
-                    items: [{sku: 'sku_FsPrnEG4pZZACP', quantity: 1}],
+            // When the customer clicks on the button, redirect
+            // them to Checkout.
+            stripe.redirectToCheckout({
+                items: [{sku: 'sku_FsPrnEG4pZZACP', quantity: 1}],
 
-                    // Do not rely on the redirect to the successUrl for fulfilling
-                    // purchases, customers may not always reach the success_url after
-                    // a successful payment.
-                    // Instead use one of the strategies described in
-                    // https://stripe.com/docs/payments/checkout/fulfillment
-                    successUrl: 'https://php.coach/thanks.html',
-                    cancelUrl: 'https://php.coach/oops.html',
-                })
-                    .then(function (result) {
-                        if (result.error) {
-                            // If `redirectToCheckout` fails due to a browser or network
-                            // error, display the localized error message to your customer.
-                            var displayError = document.getElementById('error-message');
-                            displayError.textContent = result.error.message;
-                        }
-                    });
-            });
+                // Do not rely on the redirect to the successUrl for fulfilling
+                // purchases, customers may not always reach the success_url after
+                // a successful payment.
+                // Instead use one of the strategies described in
+                // https://stripe.com/docs/payments/checkout/fulfillment
+                successUrl: 'https://php.coach/thanks.html',
+                cancelUrl: 'https://php.coach/oops.html',
+            })
+                .then(function (result) {
+                    if (result.error) {
+                        // If `redirectToCheckout` fails due to a browser or network
+                        // error, display the localized error message to your customer.
+                        var displayError = document.getElementById('error-message');
+                        displayError.textContent = result.error.message;
+                    }
+                });
         });
     });
 })();
